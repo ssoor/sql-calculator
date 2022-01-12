8fc8ccdb669f478dbe116662152472fd7eaa4f85
nieml01 <nieml01@mingyuanyun.com>
2022-01-12 09:27:20 +0000

    feature: add diff command

--- cmd/diff.go
+++ cmd/diff.go
@@ -1,3 +1,45 @@
 package cmd
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sql-calculator/diff"
+	"sql-calculator/utils"
 
+	"github.com/spf13/cobra"
+)
+
+var DiffCmd = &cobra.Command{
+	Use:   "diff [Source SQL filename](string) [Target SQL filename](string)",
+	Args:  cobra.MinimumNArgs(2),
+	Short: "SQL Diff",
+	Example: "   ./sql-calculator diff ./source.sql ./target.sql\n" +
+		"Output:\n   ALTER TABLE `db1`.`qk_t2` COMMENT = '注释被修改'",
+	Long: "SQL Diff - Compare the differences between the two SQL content and output the synchronization script",
+	Run: func(cmd *cobra.Command, args []string) {
+		sourceSql, err := ioutil.ReadFile(args[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		targetSql, err := ioutil.ReadFile(args[1])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		alters, err := diff.GetDiffFromSqlFile("", string(sourceSql), string(targetSql))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		modifySql := ""
+		for _, alter := range alters {
+			sql, _ := utils.RestoreToSql(alter)
+			modifySql += sql + ";\n"
+		}
+		fmt.Println(modifySql)
+	},
+}
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
 	"sql-calculator/cmd"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	var rootCmd = &cobra.Command{}
+	rootCmd.AddCommand(cmd.DiffCmd)
 	rootCmd.AddCommand(cmd.FingerprintCmd)
 	rootCmd.Execute()
 }
